89ba37c29162bbc647d2ac906d55c3c4366e8954
li_j604 <775837740@qq.com>
2021-12-01 12:55:28 +0800

    添加title

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/works" : "/",
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "李骏同学的作品集";
+      return args;
+    });
+  },
 };
